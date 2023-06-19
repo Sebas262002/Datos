dd3f8b2010e812dc623645f3145ccd3303c622ad
Kevin L <sebas-churuchumbi@hotmail.com>
2023-06-19 09:58:38 -0500

    cambios

--- trabajo.cpp
+++ trabajo.cpp
@@ -1,44 +1,38 @@
 #include <iostream>
-#include <list>
+using namespace std;
+
+// Definición de la estructura Nodo
+struct Nodo {
+    int valor;
+    Nodo* siguiente;
+};
 
 int main() {
-    // Crear una lista vacía de enteros
-    std::list<int> miLista;
+    // Creamos los nodos
+    Nodo* primero = new Nodo();
+    Nodo* segundo = new Nodo();
+    Nodo* tercero = new Nodo();
 
-    // Agregar elementos a la lista
-    miLista.push_back(1);
-    miLista.push_back(2);
-    miLista.push_back(3);
+    // Asignamos valores y referencias
+    primero->valor = 1;
+    primero->siguiente = segundo;
+    segundo->valor = 2;
+    segundo->siguiente = tercero;
+    tercero->valor = 3;
+    tercero->siguiente = NULL;
 
-    // Recorrer la lista e imprimir cada elemento
-    std::cout << "Elementos de la lista: ";
-    for (auto it = miLista.begin(); it != miLista.end(); ++it) {
-        std::cout << *it << " ";
+    // Recorremos la lista e imprimimos los valores
+    Nodo* actual = primero;
+    while (actual != NULL) {
+        cout << actual->valor << " ";
+        actual = actual->siguiente;
     }
-    std::cout << std::endl;
+    cout << endl;
 
-    // Eliminar un elemento de la lista
-    miLista.erase(miLista.begin());
-
-    // Insertar un elemento en la lista
-    miLista.insert(miLista.begin(), 4);
-
-    // Recorrer la lista e imprimir cada elemento nuevamente
-    std::cout << "Elementos de la lista después de eliminar e insertar: ";
-    for (auto it = miLista.begin(); it != miLista.end(); ++it) {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-
-    // Ordenar la lista
-    miLista.sort();
-
-    // Recorrer la lista e imprimir cada elemento una vez más
-    std::cout << "Elementos de la lista ordenados: ";
-    for (auto it = miLista.begin(); it != miLista.end(); ++it) {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+    // Liberamos la memoria
+    delete primero;
+    delete segundo;
+    delete tercero;
 
     return 0;
 }
